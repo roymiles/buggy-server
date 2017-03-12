6c8e85d3b9300b8ba0e37e84aab0abf9d8fe9672
iyop45 <roy.miles12@gmail.com>
2017-03-12 15:21:48 +0000

    getArduinoData and getMazeLayout

--- basestation/main.py
+++ basestation/main.py
@@ -1,10 +1,10 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import json , pprint
 basestation = Flask(__name__)  # creates object for web page
 #global variables
-xx =2
-yy =15
+xx = 2
+yy = 15
 targetinfo = {'target1': {'V2': None}, 'target2': {'R1': None, 'R2': None}, 'target3': {'R2': None, 'R3': None},
                   'target4': {'R1': None, 'R3': None}, 'target5': {'R1': None, 'C1': None, 'Freq': None},
                   'target6': {'R1': None, 'C1': None, 'Freq': None},
@@ -12,22 +12,25 @@ targetinfo = {'target1': {'V2': None}, 'target2': {'R1': None, 'R2': None}, 'tar
 
 with open('arduino_data.txt', 'w') as outfile:
         json.dump(targetinfo, outfile)
+
+# Create an empty dictionary for the arena 11 by 15
 arena = dict()
 for y in range(0, 11):
     arena[y] = dict()
     for x in range(0, 15):
         arena[y][x] = "0"
+
 with open('maze_layout.txt', 'w') as outfile:
     json.dump(arena, outfile)
-#routing
+
+# Routing
 @basestation.route('/')   # routes to home page
 def website():
-
     return user_interface()
 
+# Return 'start' or 'none' depending on whether started.txt contains the character 1 or not
 @basestation.route('/cmd')
 def cmd():
-
     started = 0
     with open('started.txt', 'r') as f:
         started = int(f.readline())
@@ -37,10 +40,12 @@ def cmd():
         return 'start'
     else:
         return 'none'
+
 @basestation.route('/ARENA')
 def arena_layout():
     with open('arduino_data.txt', 'r') as infile:
         arena = json.load(infile)
+
     return '0'
 
 @basestation.route('/VICTORY')
@@ -62,7 +67,6 @@ def victory_roll_notifier():
 
 @basestation.route('/start')
 def start():
-
     with open('started.txt', 'w') as f:
         f.write(str(1))
         f.close()
@@ -71,7 +75,6 @@ def start():
 
 @basestation.route('/stop')
 def stop():
-
     with open('started.txt', 'w') as f:
         f.write(str(0))
         f.close()
@@ -82,10 +85,11 @@ def stop():
 def update():
     with open('maze_layout.txt', 'r') as infile:
         arena = json.load(infile)
+
     position = request.args.get('position')
     orientation = int(request.args.get('orientation'))
-    xx = int(position[0]+ position[1])
-    yy = int(position[2]+ position[3])
+    xx = int(position[0] + position[1])
+    yy = int(position[2] + position[3])
     if orientation == 0:
         if arena[str(yy)][str(xx)] == "2":
             arena[str(yy)][str(xx)] = "5"
@@ -107,8 +111,6 @@ def update():
 
 @basestation.route('/ANALYSIS')
 def analysis():
-
-
     with open('arduino_data.txt', 'r') as infile:
         targetinfo = json.load(infile)
 
@@ -174,6 +176,22 @@ def user_interface():
     with open('maze_layout.txt', 'r') as infile:
         gridlayout = json.load(infile)
     return render_template("website.html", infoset=temp_vars,gridlayout=gridlayout) # runs website template written in html using temp vars dictionary as an argument for interrogated info
+
+@basestation.route('/getArduinoData')
+def getArduinoData():
+    with open('arduino_data.txt', 'r') as infile:
+        arduinoData = json.load(infile)
+
+    return jsonify(arduinoData)
+
+
+@basestation.route('/getMazeLayout')
+def getMazeLayout():
+    with open('maze_layout.txt', 'r') as infile:
+        mazeLayout = json.load(infile)
+
+    return jsonify(mazeLayout)
+
 
 
 if __name__ == "__main__":  # checks if flask is running
